c7bc2ddf043a3da4ac17418d9beddbe87d28b31a
seunghyonpark <creath.park@gmail.com>
2025-03-09 14:50:09 +0900

    refactor: remove .env.example and update anvil.ts to define Polygon chain

--- src/lib/anvil.ts
+++ src/lib/anvil.ts
@@ -1,5 +1,7 @@
 import { defineChain } from "thirdweb"
 
+
+/*
 export default defineChain({
     id: 31337,
     name: "Anvil",
@@ -8,6 +10,26 @@ export default defineChain({
     nativeCurrency: {
         name: "Anvil Ether",
         symbol: "ETH",
+        decimals: 18,
+    },
+});
+*/
+
+
+// polygon mainnet
+
+export default defineChain({
+    id: 137,
+    name: "Polygon",
+    chain: "polygon",
+    shortName: "matic",
+    chainId: 137,
+    slug: "polygon",
+    rpc: ["https://rpc-mainnet.maticvigil.com"],
+    testnet: false,
+    nativeCurrency: {
+        name: "Matic",
+        symbol: "MATIC",
         decimals: 18,
     },
 });
